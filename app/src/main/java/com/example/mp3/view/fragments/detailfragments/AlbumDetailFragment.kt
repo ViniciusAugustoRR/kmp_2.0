9ddaf1d04bb858c6d47a7382771c1321219fc99e
ViniciusAugustoRR <vini.rios.rocha@gmail.com>
2023-10-12 22:08:57 -0300

    TabLayour + ViewPager Visualização de listas de albums e artistas Player tocando corretamente

--- app/src/main/java/com/example/mp3/view/fragments/detailfragments/AlbumDetailFragment.kt
+++ app/src/main/java/com/example/mp3/view/fragments/detailfragments/AlbumDetailFragment.kt
@@ -1,33 +1,24 @@
 package com.example.mp3.view.fragments.detailfragments
 
+import android.media.MediaMetadataRetriever
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.mp3.R
+import com.example.mp3.databinding.FragmentAlbumDetailBinding
+import com.example.mp3.databinding.FragmentArtistDetailBinding
+import com.example.mp3.logic.viewmodels.PlayerVM
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AlbumDetailFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AlbumDetailFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding : FragmentAlbumDetailBinding
+    private val mmr = MediaMetadataRetriever()
+    lateinit var playerVM: PlayerVM
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -35,26 +26,8 @@ class AlbumDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_album_detail, container, false)
+        val view = inflater.inflate(R.layout.fragment_album_detail, container, false)
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AlbumDetailFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AlbumDetailFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
